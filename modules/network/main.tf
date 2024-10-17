9b72f3d7221931e75396617da04c3624d9aab721
Simon <106014761+simonhoumb@users.noreply.github.com>
2024-10-17 23:04:25 +0200

    Added network module features

---
+++ modules/network/main.tf
@@ -0,0 +1,56 @@
+resource "azurerm_network_security_group" "nsg" {
+  name                = var.nsg_name
+  location            = var.location
+  resource_group_name = var.rg_name
+  security_rule {
+    name                       = "tcp-inbound-p22"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  tags = local.common_tags
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  location            = var.location
+  resource_group_name = var.rg_name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+  tags                = local.common_tags
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = var.rg_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet_network_security_group_association" "snnsg" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_public_ip" "lb_public_ip" {
+  name                = "PublicIPForLB"
+  location            = var.location
+  resource_group_name = var.rg_name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_lb" "lb" {
+  name                = var.lb_name
+  location            = var.location
+  resource_group_name = var.rg_name
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.lb_public_ip.id
+  }
+}
--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,1 +1,35 @@
+variable "rg_name" {
+  type        = string
+  description = "Name of Resource Group."
+  default     = "rg-${local.common_tags.owner}-${local.common_tags.project}-${local.common_tags.environment}-${var.location}"
+}
 
+variable "location" {
+  type        = string
+  description = "Location of Resource Group/Resource."
+  default     = "westeurope"
+}
+
+variable "nsg_name" {
+  type        = string
+  description = "Name of Network Security Group."
+  default     = "nsg-${local.common_tags.owner}-${local.common_tags.project}-${local.common_tags.environment}-${var.location}"
+}
+
+variable "vnet_name" {
+  type        = string
+  description = "Name of Virtual Network."
+  default     = "vnet-${local.common_tags.owner}-${local.common_tags.project}-${local.common_tags.environment}-${var.location}"
+}
+
+variable "subnet_name" {
+  type        = string
+  description = "Name of Subnet."
+  default     = "subnet-${local.common_tags.owner}-${local.common_tags.project}-${local.common_tags.environment}-${var.location}"
+}
+
+variable "lb_name" {
+  type        = string
+  description = "Name of Load Balancer."
+  default     = "lb-${local.common_tags.owner}-${local.common_tags.project}-${local.common_tags.environment}-${var.location}"
+}
