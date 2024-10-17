58fac306454837f400e93920645860f72f2a01e2
Simon <106014761+simonhoumb@users.noreply.github.com>
2024-10-17 23:04:01 +0200

    Added main resource group and variables

--- variables.tf
+++ variables.tf
@@ -3,3 +3,15 @@ variable "subscription_id" {
   description = "Azure subscription ID required for use."
   default     = ""
 }
+
+variable "rg_name" {
+  type        = string
+  description = "Name of Resource Group."
+  default     = "rg-${local.common_tags.owner}-${local.common_tags.project}-${local.common_tags.environment}-${var.location}"
+}
+
+variable "location" {
+  type        = string
+  description = "Location of Resource Group/Resource."
+  default     = "westeurope"
+}
