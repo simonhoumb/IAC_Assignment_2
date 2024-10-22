80839c6655ac6568e5448543629592393e828a79
Simon <106014761+simonhoumb@users.noreply.github.com>
2024-10-22 10:44:01 +0200

    Fixed checkov severities for storage account

--- backend/main.tf
+++ backend/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rg_backend" {
-  name     = "${var.rg_backend_name}-${local.common_tags.owner}-${local.common_tags.project}-${local.common_tags.environment}-${var.backend_location}"
+  name     = "${var.rg_backend_name}-${local.common_tags.owner}-${local.common_tags.backend}-${local.common_tags.project}-${local.common_tags.environment}-${var.backend_location}"
   location = var.backend_location
   tags     = local.common_tags
 }
@@ -11,12 +11,35 @@ resource "random_string" "random_string" {
 }
 
 resource "azurerm_storage_account" "sa_backend" {
-  name                     = "${lower(var.sa_backend_name)}${local.common_tags.project}${local.common_tags.environment}${random_string.random_string.result}"
-  resource_group_name      = azurerm_resource_group.rg_backend.name
-  location                 = azurerm_resource_group.rg_backend.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-  tags                     = local.common_tags
+  name                            = "${lower(var.sa_backend_name)}${local.common_tags.project}${local.common_tags.environment}${random_string.random_string.result}"
+  resource_group_name             = azurerm_resource_group.rg_backend.name
+  location                        = azurerm_resource_group.rg_backend.location
+  account_tier                    = "Standard"
+  account_replication_type        = "GRS"
+  min_tls_version                 = "TLS1_2"
+  public_network_access_enabled   = false
+  allow_nested_items_to_be_public = false
+  local_user_enabled              = false
+  shared_access_key_enabled       = false
+  sas_policy {
+    expiration_period = "90.00:00:00"
+    expiration_action = "Log"
+  }
+  blob_properties {
+    delete_retention_policy {
+      days = 7
+    }
+  }
+  queue_properties {
+    logging {
+      delete                = true
+      read                  = true
+      write                 = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+  }
+  tags = local.common_tags
 }
 
 resource "azurerm_storage_container" "sa_backend_container" {
--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -4,13 +4,36 @@ resource "random_string" "random_string" {
   upper   = false
 }
 
-resource "azurerm_storage_account" "sa" {
-  name                     = "${lower(var.sa_name)}${random_string.random_string.result}"
-  resource_group_name      = var.rg_name
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  tags                     = local.common_tags
+resource "azurerm_storage_account" "sa_backend" {
+  name                            = "${lower(var.sa_name)}${local.common_tags.project}${local.common_tags.environment}${random_string.random_string.result}"
+  resource_group_name             = azurerm_resource_group.rg_backend.name
+  location                        = azurerm_resource_group.rg_backend.location
+  account_tier                    = "Standard"
+  account_replication_type        = "GRS"
+  min_tls_version                 = "TLS1_2"
+  public_network_access_enabled   = false
+  allow_nested_items_to_be_public = false
+  local_user_enabled              = false
+  shared_access_key_enabled       = false
+  sas_policy {
+    expiration_period = "90.00:00:00"
+    expiration_action = "Log"
+  }
+  blob_properties {
+    delete_retention_policy {
+      days = 7
+    }
+  }
+  queue_properties {
+    logging {
+      delete                = true
+      read                  = true
+      write                 = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+  }
+  tags = local.common_tags
 }
 
 resource "azurerm_storage_container" "image_container" {
