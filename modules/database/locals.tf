34dc0917b69dc814953e7d7e4cbd2ec8661a4d08
Simon <106014761+simonhoumb@users.noreply.github.com>
2024-10-25 13:56:26 +0200

    Removed interpolation-only expression

--- modules/database/locals.tf
+++ modules/database/locals.tf
@@ -3,7 +3,7 @@ locals {
     company     = "OperaTerraAS"
     project     = "oppg2"
     owner       = "simonhou"
-    environment = "${terraform.workspace}"
+    environment = terraform.workspace
   }
   naming_suffix_seperated     = "-${local.common_tags.owner}-${local.common_tags.project}-${local.common_tags.environment}"
   naming_suffix_not_seperated = "${local.common_tags.owner}${local.common_tags.project}${local.common_tags.environment}"
