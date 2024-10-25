f6b6f2c53f1df5359c16b0a44d02f1dc89e57621
Simon <106014761+simonhoumb@users.noreply.github.com>
2024-10-25 12:51:46 +0200

    Added naming suffix to locals

--- locals.tf
+++ locals.tf
@@ -5,4 +5,6 @@ locals {
     owner       = "simonhou"
     environment = "${terraform.workspace}"
   }
+  naming_suffix_seperated     = "-${local.common_tags.owner}-${local.common_tags.project}-${local.common_tags.environment}"
+  naming_suffix_not_seperated = "${local.common_tags.owner}${local.common_tags.project}${local.common_tags.environment}"
 }
