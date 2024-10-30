5741ce6c43072159acd045d17b005e6898b6c50f
Simon <106014761+simonhoumb@users.noreply.github.com>
2024-10-30 10:42:36 +0100

    Removed ssh security group rule

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -3,17 +3,6 @@ resource "azurerm_network_security_group" "nsg" {
   location            = var.location
   resource_group_name = var.rg_name
   security_rule {
-    name                       = "tcp-inbound-p22"
-    priority                   = 1022
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = var.source_address_prefix
-    destination_address_prefix = "*"
-  }
-  security_rule {
     name                       = "tcp-inbound-p443"
     priority                   = 1043
     direction                  = "Inbound"
