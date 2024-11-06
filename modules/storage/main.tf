30545249f88049a210feebbd2d4a6b4ee7e92471
Simon <106014761+simonhoumb@users.noreply.github.com>
2024-11-06 12:51:09 +0100

    Changed storage container name to only use lowercase and hyphens

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -33,7 +33,7 @@ resource "azurerm_storage_account" "sa" {
 }
 
 resource "azurerm_storage_container" "image_container" {
-  name                  = "product_images"
+  name                  = "product-images"
   storage_account_name  = azurerm_storage_account.sa.name
   container_access_type = "private"
 }
